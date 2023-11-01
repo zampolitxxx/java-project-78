9a3310ffec019ab17b18faa45c8068859970f87a
zampolitxxx <zampolitxxx@gmail.com>
2023-11-01 13:24:44 +0900

    add tests

--- app/src/test/java/hexlet/code/MainTest.java
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -1,4 +1,40 @@
 package hexlet.code;
 
-public class MainTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    private StringSchema schema;
+    private Validator v;
+    @BeforeEach
+    void initializing() {
+        v = new Validator();
+        schema = v.string();
+    }
+
+    @Test
+    void nullTest() {
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid(null));
+        schema.required();
+        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null));
+    }
+
+    @Test
+    void containsTest() {
+        assertTrue(schema.contains("wh").isValid("what does the fox say"));
+        assertTrue(schema.contains("what").isValid("what does the fox say"));
+        assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
+        assertFalse(schema.isValid("what does the fox say"));
+    }
+
+    @Test
+    void minLengthTest() {
+
+    }
+
 }
