d95ab3533873a45a8a24c3d025f7de087f671762
zampolitxxx <zampolitxxx@gmail.com>
2023-11-01 16:17:37 +0900

    remove unused import from NumberTest
    remove Unused import - java.util.function.BooleanSupplier

--- app/src/test/java/hexlet/code/NumberTest.java
+++ app/src/test/java/hexlet/code/NumberTest.java
@@ -2,9 +2,6 @@ package hexlet.code;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.function.BooleanSupplier;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
